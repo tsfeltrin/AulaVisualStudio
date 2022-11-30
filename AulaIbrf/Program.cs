a413dd860af9136c7d069eda14aa77de91c6bf32
Tais <tsfeltrin@gmail.com>
2022-11-30 20:04:26 -0300

    Inserindo Repositorio

---
+++ AulaIbrf/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace AulaIbrf
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ola Mundo!");
+        }
+    }
+}
